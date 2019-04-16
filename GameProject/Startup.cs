da6a65b3017676c40c3195f161fc0e43823d140e
Ian Noyes <noyese18@hotmail.com>
2019-04-16 15:14:32 -0700

    Add project files.

---
+++ GameProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameProject.Startup))]
+namespace GameProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
